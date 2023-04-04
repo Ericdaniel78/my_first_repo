3fe889ccba77b0f29843c9f3b05ee9f402f5afb2
Ericdaniel78 <eric_daniel78@ciencias.unam.mx>
2023-04-04 11:14:28 -0600

    Update

--- git_access.R
+++ git_access.R
@@ -1,4 +1,15 @@
 # Acceso a GIt
 
+library(usethis)
+use_git_config( user.name = "Ericdaniel78", user.email = "eric_daniel78@ciencias.unam.mx" )
+usethis::git_default_branch_configure()
+
 usethis::create_github_token()
 gitcreds::gitcreds_set()
+
+# Crear un ink a un repositorio
+
+# usethis::create_from_github(
+#   "https://github.com/Ericdaniel78/my_first_repo.git",
+#   destdir = "~/local/directory"
+# )
