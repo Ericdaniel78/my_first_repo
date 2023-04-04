f00614380b9579cae0f0b75313b75bdccbceb108
Ericdaniel78 <eric_daniel78@ciencias.unam.mx>
2023-04-04 11:14:28 -0600

    Previous code

--- git_access.R
+++ git_access.R
@@ -1,4 +1,8 @@
 # Acceso a GIt
 
+library(usethis)
+use_git_config( user.name = "Ericdaniel78", user.email = "eric_daniel78@ciencias.unam.mx" )
+usethis::git_default_branch_configure()
+
 usethis::create_github_token()
 gitcreds::gitcreds_set()
